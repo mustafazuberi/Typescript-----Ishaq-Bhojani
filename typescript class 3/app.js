998906296a960069415efbb6d1dd557d5eb11b44
MustafaRazaZuberi <mustafazuberi986@gmail.com>
2023-08-24 08:42:40 -0700

    5 classes done

--- typescript class 3/app.js
+++ typescript class 3/app.js
@@ -1,5 +1,20 @@
 // var input1 = document.getElementById("input1")! as HTMLInputElement;
 // // yaha ! jabhi likha ha Q-K  hum TS ko bta rehy hain ke ya variable undefined nahi rehga aisi Id mojudd ha or ya value aigi
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // var input2 = document.getElementById("input2")! as HTMLInputElement;
 // var button = document.getElementById("btn")!;
 // var add = (val1: number, val2: number) => {
@@ -46,6 +61,103 @@ var add = function (val1, val2, calc) {
     else if (calc === "substract") {
         return val1 - val2;
     }
+    return 0;
 };
 console.log(add(10, 5, "add"));
 console.log(add(10, 5, "substract"));
+// -------------Tuples (jab fix lenght off arrray chahye ho tw tuples use krty hain )
+var gender = ["Male", "Female"];
+// gender[2] = 'dsada'       // yaha problem krega yay qk hum ne define kya hua ha ke 2 sa zyada values nahi hosakti
+// gender.push('dasdasd')      // but  ya yaha problem nahi krega qk ya typescript ka bug h
+// class Student {
+//   public stName : string;
+//   private rollNo : number;
+//   constructor (stName : string , rollNo : number){
+//     this.stName = stName
+//     this.rollNo = rollNo
+//   }
+// }
+// const student1 = new Student("Yasir",1234)
+// console.log(student1)
+// console.log(student1.stName)
+var Student = /** @class */ (function () {
+    function Student(stName, rollNo) {
+        this.stName = stName;
+        this.rollNo = rollNo;
+        this.skills = [];
+    }
+    Student.prototype.addSkill = function (skill) {
+        this.skills.push(skill);
+    };
+    return Student;
+}());
+var student1 = new Student("Yasir", 1234);
+console.log(student1.stName);
+student1.addSkill("javascript");
+console.log(student1);
+var Product = /** @class */ (function () {
+    function Product(_id, _username, _price) {
+        this._id = _id;
+        this._username = _username;
+        this._price = _price;
+    }
+    Object.defineProperty(Product.prototype, "id", {
+        get: function () {
+            return this._id;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Product.prototype, "username", {
+        get: function () {
+            return this._username;
+        },
+        set: function (newName) {
+            if (!newName) {
+                throw new Error("Name cannot be empty");
+            }
+            this._username = newName;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Product.prototype, "price", {
+        get: function () {
+            return this._price;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Product;
+}());
+var ClothingProduct = /** @class */ (function (_super) {
+    __extends(ClothingProduct, _super);
+    function ClothingProduct(id, username, price, _color, _size) {
+        var _this = _super.call(this, id, username, price) || this;
+        _this._color = _color;
+        _this._size = _size;
+        return _this;
+    }
+    return ClothingProduct;
+}(Product));
+var ElectronicProduct = /** @class */ (function (_super) {
+    __extends(ElectronicProduct, _super);
+    function ElectronicProduct(id, username, price, _brand, _model) {
+        var _this = _super.call(this, id, username, price) || this;
+        _this._brand = _brand;
+        _this._model = _model;
+        return _this;
+    }
+    return ElectronicProduct;
+}(Product));
+// const product1 = new Product(1, "Mustafa", 90000);
+// console.log(product1);
+// console.log(product1.id);
+// console.log(product1.price);
+// console.log(product1.username);
+// product1.username = "setting new name";
+// console.log(product1.username);
+var tShirt = new ClothingProduct(1, "Mustafa", 90000, "Red", "M");
+console.log(tShirt);
+var fridge = new ElectronicProduct(1, "Mustafa", 90000, "Dawlance", 2020);
+console.log(tShirt);
--- typescript class 3/app.ts
+++ typescript class 3/app.ts
@@ -78,25 +78,222 @@ console.log(persons);
 type Calc = "add" | "substract";
 type Add = (val1: number, val2: number, calc: Calc) => number;
 
-const add = function (val1: number, val2: number, calc: Calc) : number  {
+const add = function (val1: number, val2: number, calc: Calc): number {
   if (calc === "add") {
     return val1 + val2;
   } else if (calc === "substract") {
     return val1 - val2;
   }
-  return 0
+  return 0;
 };
 
 console.log(add(10, 5, "add"));
 console.log(add(10, 5, "substract"));
 
-
-
-
-
-
-
 // -------------Tuples (jab fix lenght off arrray chahye ho tw tuples use krty hain )
-let gender: [string,string] = ['Male','Female']
+let gender: [string, string] = ["Male", "Female"];
 // gender[2] = 'dsada'       // yaha problem krega yay qk hum ne define kya hua ha ke 2 sa zyada values nahi hosakti
-// gender.push('dasdasd')      // but  ya yaha problem nahi krega qk ya typescript ka bug h 
+// gender.push('dasdasd')      // but  ya yaha problem nahi krega qk ya typescript ka bug h
+
+// class Student {
+//   public stName : string;
+//   private rollNo : number;
+//   constructor (stName : string , rollNo : number){
+//     this.stName = stName
+//     this.rollNo = rollNo
+//   }
+// }
+
+// const student1 = new Student("Yasir",1234)
+// console.log(student1)
+// console.log(student1.stName)
+
+class Student {
+  private readonly skills: string[] = [];
+  constructor(public stName: string, public readonly rollNo: number) {}
+
+  addSkill(skill: string) {
+    this.skills.push(skill);
+  }
+}
+
+const student1 = new Student("Yasir", 1234);
+console.log(student1.stName);
+student1.addSkill("javascript");
+console.log(student1);
+
+class Product {
+  constructor(
+    private _id: number,
+    private _username: string,
+    private _price: number
+  ) {}
+  get id() {
+    return this._id;
+  }
+  get username() {
+    return this._username;
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  set username(newName: string) {
+    if (!newName) {
+      throw new Error("Name cannot be empty");
+    }
+    this._username = newName;
+  }
+}
+
+class ClothingProduct extends Product {
+  constructor(
+    id: number,
+    username: string,
+    price: number,
+    private _color: string,
+    private _size: "S" | "M" | "L" | "XL"
+  ) {
+    super(id, username, price);
+  }
+}
+
+class ElectronicProduct extends Product {
+  constructor(
+    id: number,
+    username: string,
+    price: number,
+    private _brand: string,
+    private _model: number
+  ) {
+    super(id, username, price);
+  }
+}
+
+// const product1 = new Product(1, "Mustafa", 90000);
+// console.log(product1);
+// console.log(product1.id);
+// console.log(product1.price);
+// console.log(product1.username);
+// product1.username = "setting new name";
+
+// console.log(product1.username);
+
+const tShirt = new ClothingProduct(1, "Mustafa", 90000, "Red", "M");
+console.log(tShirt);
+
+const fridge = new ElectronicProduct(1, "Mustafa", 90000, "Dawlance", 2020);
+console.log(tShirt);
+
+// Is class ke 2 instance hogeye Qk is se 2 instance bne hain
+
+// Agar hum chahatey hain ke Is ka multiple instance naa create hoo
+// 2 hum Iske constructor ko private kr denge or HUm agar class ki value lekni hogi
+// tw static methods banainge ----- Ye methods hamari class se connected hote hain lekin hamare instance se
+// disconnected hain
+
+// -------------------------Singeltons----------------
+
+class Utility {
+  static instance: Utility;
+  private constructor() {}
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new Utility();
+    }
+    return this.instance;
+  }
+}
+const util1 = Utility.getInstance();
+
+// Is se hamesha ek hee instance create hoga 2nd pr bh phli dfa create hone wala hee return hojaiga
+
+// Aese agar humkarein ke apne object ki type 2 me se koi ek de sakein tw ise Union Types kehte hain
+// --------------Union Types--------------
+// type Animal = {
+//   name: string;
+//   runningSpeed: number;
+// };
+
+// type Bird = {
+//   name: string;
+//   flyingSpeed: number;
+// };
+
+// type Creature = Animal | Bird;
+
+// const tiger: Creature = { name: "tiger", runningSpeed: 321 };
+
+// ----------------------------------------Intersection Types----------------------------------------
+
+// type Animal2 = {
+//   name: string;
+//   runningSpeed: number;
+// };
+
+// type Bird2 = {
+//   name: string;
+//   flyingSpeed: number;
+// };
+
+// type Creature2 = Animal2 & Bird2;
+
+// const tiger2: Creature2 = {
+//   name: "tiger",
+//   runningSpeed: 321,
+//   flyingSpeed: 2332,
+// };
+
+// ----------------------Interfaces----------------
+
+// interface Animal {
+//   name: string;
+//   runningSpeed: number;
+// }
+// interface Bird {
+//   name: string;
+//   flyingSpeed: number;
+// }
+
+// interface Creature extends Animal, Bird {}
+
+// const tiger: Creature = { name: "tiger", runningSpeed: 3232, flyingSpeed: 342 };
+
+// interface Animal {
+//   name: string;
+//   runningSpeed: number;
+//   eat(food: string): string;
+// }
+
+// class Tiger implements Animal {
+//   constructor(public name: string, public runningSpeed: number) {}
+
+//   eat(food: string): string {
+//     return food;
+//   }
+// }
+
+
+
+
+// Type Guards 
+
+type Animal = {
+  name: string;
+  runningSpeed: number;
+}
+type Bird = {
+  name: string;
+  flyingSpeed: number;
+}
+
+type Creature = Animal | Bird 
+
+function log(creature : Creature){
+  if('runningSpeed' in creature){ // Type Guards
+    console.log(creature.runningSpeed)
+  }
+}
+
+
